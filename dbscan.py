e024b0ec2302c99d2273f2749297d89b288a673b
jpeone <jesse.gene.peone@gmail.com>
2020-08-21 19:11:35 -0700

    Basic class structure set up

---
+++ dbscan.py
@@ -0,0 +1,14 @@
+class DensityBasedSCAN(Object):
+
+    def __init__(self, epsilon = 1, min_samples = 3):
+        self.epsilon = epislon
+        self.min_samples = min_samples
+
+    def fit(self, data):
+        pass # TODO: fitting without returning clusters
+
+    def fit_predict(self, data):
+        pass # TODO: fitting, and returns clusters
+
+    def predict(self, data):
+        pass # TODO: maybe this is just returning clusters for only fit data
